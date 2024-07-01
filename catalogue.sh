bfc0103f09040b4fb8474f78142380e8f53c93e5
santhoshkumar1122 <santhosh22.vtalent@gmail.com>
2024-07-01 02:57:48 -0700

    committed

--- catalogue.sh
+++ catalogue.sh
@@ -34,7 +34,7 @@ dnf remove nodejs -y &>> $LOGFILE
 
 VALIDATE $? "Disabling current NodeJS"
 
-dnf module install nodejs:18 -y  &>> $LOGFILE
+dnf module enable nodejs:18 -y  &>> $LOGFILE
 
 VALIDATE $? "Enabling NodeJS:18"
 
